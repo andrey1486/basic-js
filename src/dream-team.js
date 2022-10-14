489e2719635db7ce1ad780f5ada1aa70f03c63a0
andrey1486 <chervonukandrey@gmail.com>
2022-10-14 14:26:44 +0300

    dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,18 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
- * @return {String | Boolean} name of the team or false
- * in case of incorrect members
- *
- * @example
- * 
- * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
- * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
- *
- */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let teamName = [];
+  if (!Array.isArray(members) || !Array.from(members)) {
+    return false;
+  }
+  for (let name of members) {
+    if (typeof name !== 'string') {
+      continue;
+    } else {
+      teamName.push(name.trim()[0]);
+    }
+  }
+  return teamName.map(item => item.toUpperCase()).sort().join('');
 }
 
 module.exports = {
