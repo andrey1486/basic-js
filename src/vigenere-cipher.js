0c189dbed3a390f6c9f4e9c42c57a6fe606bae56
andrey1486 <88458447+andrey1486@users.noreply.github.com>
2022-10-14 15:42:44 +0300

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -18,15 +18,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
- */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ */class VigenereCipheringMachine {
+  constructor(mode = true) {
+    this.mode = mode;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  crypt(message, key, type) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let resultStr = '';
+    let j = 0;
+    for (let i = 0;  i < message.length; i++) {
+      if (message.charCodeAt(i) < 65 || message.charCodeAt(i) > 90) {
+        resultStr += message[i];
+      } else {
+        if (type) {
+          resultStr += String.fromCharCode((message.charCodeAt(i) + key.charCodeAt(j % key.length) - 2 * 65) % 26 + 65);
+        } else {
+          resultStr += String.fromCharCode((message.charCodeAt(i) - key.charCodeAt(j % key.length) + 26) % 26 + 65);
+        }
+        j++;
+      }
+    }
+    return this.mode ? resultStr : resultStr.split('').reverse().join('');
+  }
+  encrypt(message, key) {
+    return this.crypt(message, key, true);
+  }
+  decrypt(encryptedMessage, key) {
+    return this.crypt(encryptedMessage, key, false);
   }
 }
 
